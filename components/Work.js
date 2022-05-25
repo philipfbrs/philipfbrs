470287ac4cfeaa09b0cb2ed31f9155d798900d67
philipfbrs <philipfbrs@gmail.com>
2022-05-25 20:38:53 +0800

    added work 2

--- components/Work.js
+++ components/Work.js
@@ -46,21 +46,29 @@ export const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url($"./assets/realestate.jpg")` }}
+            style={{ backgroundImage: `url("pizza_shop.jpg")` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                React JS Application
+              <span className="text-2xl font-bold text-white tracking-wider ">
+                Kuta's Pizza Shop
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://kuta-pizza.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
-                </a>
-                <a href="/">
+                </a>{" "}
+                <a
+                  href="https://github.com/philipfbrs/kuta-pizza"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
